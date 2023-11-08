a5a6098d52fadc22d4ee4be0d74cce5b6bb4e01e
ThomasRask123 <thra22ac@student.cbs.dk>
2023-11-08 11:31:11 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,48 +1,11 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-
+const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const host = 'localhost';
-const path = require('path')
-const userRoute = require("./routes/userRoute");
-
-app.use(express.static(__dirname + '../client'));
-
-app.use("/", userRoute);
-
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/pages/home.html'));
-  
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-app.get('/home.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/scripts/home.js'));
-  
-});
-
-app.get('/global.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/styles/global.css'));
-  
-});
-
-
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-  socket.on('user joined', username => {
-    console.log(username + " joined the chat")
-    io.emit('chat message', username + " joined the chat");
-  });
-});
-
-http.listen(port, host, () => {
-  console.log(`Socket.IO server running at http://${host}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
